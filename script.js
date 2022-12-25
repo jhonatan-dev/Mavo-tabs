e56192a7e30fd34992b902ce83a552b2a3094164
jhonatan <jhonatanmaranosilva@gmail.com>
2022-12-25 05:59:21 -0300

    update json file data

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 (async()=>{
     await Mavo.inited;
     await Mavo.all[0].dataLoaded;
-    let blocks = JSON.parse(localStorage.blocks)
-    Mavo.all[0].render(blocks);
-    console.log(Mavo.mode)
+    // let blocks = JSON.parse(localStorage.blocks)
+    // Mavo.all[0].render(blocks)
 })();
 
-window.onload = (event) => {
+const searching = () => {
 	getData(function(res) {
+		console.log('OK')
         localStorage.blocks = JSON.stringify(res)
+		Mavo.all[0].render(res)
     });
-};
+}
 
 const getData = (callback) => {
 	var settings = {
